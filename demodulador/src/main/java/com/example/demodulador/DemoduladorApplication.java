f903dca69122b85e914af97d573820f2c29cd1f3
LuisAngel <a20192832@pucp.edu.pe>
2023-08-24 20:57:45 -0500

    inicio

---
+++ demodulador/src/main/java/com/example/demodulador/DemoduladorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodulador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoduladorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoduladorApplication.class, args);
+    }
+
+}
---
+++ demodulador/src/test/java/com/example/demodulador/DemoduladorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodulador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoduladorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
